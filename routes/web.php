6096c8357e621182c49bd0cc718643ee88729a3e
Ranneciva28 <reddingtongaming28@gmail.com>
2026-08-16 18:28:25 +0700

    Add one-time admin setup routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 use App\Http\Controllers\CalculatorController;
+use App\Http\Controllers\SetupController;
 use Illuminate\Support\Facades\Route;
+
 Route::get('/', [CalculatorController::class,'index'])->name('home');
 Route::get('/calculator/{slug}', [CalculatorController::class,'show'])->name('calculator.show');
 Route::post('/calculator/{slug}', [CalculatorController::class,'calculate'])->name('calculator.calculate');
+
+Route::get('/setup', [SetupController::class, 'show'])->name('setup.show');
+Route::post('/setup', [SetupController::class, 'store'])->name('setup.store');
